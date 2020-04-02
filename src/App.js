528a85a7aaeda0680bcfbe394433e183fef19942
vaibhav <vaibhav@1var.com>
2020-04-02 17:31:20 +0530

    antdesign added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <DatePicker />
         </a>
       </header>
     </div>
